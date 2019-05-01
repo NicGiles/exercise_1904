47a1e182636a9c32a141cc1d094f8c5568f239ac
NicGiles <n.r.giles1@gmail.com>
2019-05-01 18:17:32 +0100

    Finalised date formatter. Satisfied tests pass in accordance with guidelines.

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,10 +1,23 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
 
-var today = new Date(dateToFormatTimeMillis).toDateString();
-
+var today = new Date(dateToFormatTimeMillis)
+// raw date for later formatting.
+var today_s = new Date(dateToFormatTimeMillis).toDateString();
 var date_to_check = new Date(systemDateTimeMillis).toDateString();
+// dates in string form to remove time of day difference.
 
-var result = today === date_to_check ? "TODAY" : "Not Today"
+var day = today.getDate();
+var month = today.getMonth()+1;
+var year = today.getFullYear();
+if(day<10)
+{ day='0'+day; }
+if(month<10)
+{ month='0'+month; }
+var today_formatted = day+'/'+month+'/'+year;
+// formatting date with allowances for additional 0s required for earlier days and months.
+
+var result = today_s === date_to_check ? "TODAY" : today_formatted
+// Ternary to check system date against current date.
 
 return result
 };
