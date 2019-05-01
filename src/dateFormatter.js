324d40b7e1db0d096f6b2795d8fc120ca10a09fb
NicGiles <n.r.giles1@gmail.com>
2019-05-01 17:25:18 +0100

    Working ternary if date check is today.

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,10 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+
+var today = new Date(dateToFormatTimeMillis).toDateString();
+
+var date_to_check = new Date(systemDateTimeMillis).toDateString();
+
+var result = today === date_to_check ? "TODAY" : "Not Today"
+
+return result
 };
